e33e3c0a383cb97684330845236064982d7c2284
ViktorBond7 <141341341+ViktorBond7@users.noreply.github.com>
2023-12-04 22:43:52 +0100

    task-6

---
+++ js/task-01.js
@@ -0,0 +1,14 @@
+`use strict`;
+
+const listItems = categories.children;
+console.log(`Number of categories: ${listItems.length}`);
+const categoryItemsRef = document.querySelectorAll('li.item');
+categoryItemsRef.style.color = 'red';
+
+categoryItemsRef.forEach(element => {
+  const categoriTitleRef = element.querySelector('h2').textContent;
+  const categoriElementsRef = element.querySelectorAll('ul li');
+
+  console.log(`Category: ${categoriTitleRef}`);
+  console.log(`Elements: ${categoriElementsRef.length}`);
+});
